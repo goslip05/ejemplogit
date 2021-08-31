c042a9231664e7b960867a088f5528b8b7615c4a
Diego Alexander Jimenez Vanegas <diegojimenez@Ghost.local>
2021-08-30 21:16:43 -0500

    agregando signo5

--- holamundo.java
+++ holamundo.java
@@ -2,11 +2,9 @@ public class HolaMundo
 {
 public static void main(String[]args){
 if (args.length>0)
-Sstem.out.println("Hola Mundo " +args[0]);
+Srtem.out.println("Hola Mundo " +args[0]);
 else 
 System.out.println("Hola Mundo!");
-}
-}
 
 
 		
